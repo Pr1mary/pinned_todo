dfb5c99f1ab0836d27e949147bedf8e28ef944e6
Muhammad Rafli Satriandi <raflisatriandi@gmail.com>
2020-07-19 03:14:07 +0700

    Add exsisting source code
    Add exsisting source code

--- app/src/main/java/com/se/pinned/MainActivity.java
+++ app/src/main/java/com/se/pinned/MainActivity.java
@@ -2,13 +2,60 @@ package com.se.pinned;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> taskList;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+    EditText inputTask;
+    Context currCtx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currCtx = this;
+        taskList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.task_list_layout, taskList);
+
+        listView = findViewById(R.id.task_list);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView taskView = (TextView) view;
+
+                taskList.remove(taskView.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+
+                Toast.makeText(currCtx, "Task finished!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        inputTask = findViewById(R.id.input_task);
+
+    }
+
+    public void addItem(View view){
+        taskList.add(inputTask.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+
+        inputTask.setText("");
+        Toast.makeText(currCtx, "Task Pinned!", Toast.LENGTH_SHORT).show();
     }
 }
