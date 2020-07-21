9e695bb57bc1b4feb2d82f2d2c94a250b8eb99cf
Muhammad Rafli Satriandi <raflisatriandi@gmail.com>
2020-07-21 14:49:36 +0700

    sharedpreferences added
    sharedpreferences added, but still have bugs

--- app/src/main/java/com/se/pinned/MainActivity.java
+++ app/src/main/java/com/se/pinned/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -13,9 +14,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
     Context currCtx;
 
     SharedPreferences sharedPref;
-    Set<String> taskData;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         currCtx = this;
         taskList = new ArrayList<>();
         arrayAdapter = new ArrayAdapter<>(this, R.layout.task_list_layout, taskList);
+        sharedPref = getPreferences(MODE_PRIVATE);
 
         listView = findViewById(R.id.task_list);
         listView.setAdapter(arrayAdapter);
@@ -54,8 +55,11 @@ public class MainActivity extends AppCompatActivity {
 
         inputTask = findViewById(R.id.input_task);
 
+        //load item list
+        loadItem();
     }
 
+    //add item to the list
     public void addItem(View view){
         String task = inputTask.getText().toString();
         if(!task.isEmpty()){
@@ -64,10 +68,25 @@ public class MainActivity extends AppCompatActivity {
 
             inputTask.setText("");
             Toast.makeText(currCtx, "Task Pinned!", Toast.LENGTH_SHORT).show();
+
+            //save task list to shared preferences
+            SharedPreferences.Editor prefsEditor =  sharedPref.edit();
+            Gson gson = new Gson();
+            String json = gson.toJson(taskList);
+            prefsEditor.putString("TaskData", json);
+            prefsEditor.commit();
+
         }else{
             Toast.makeText(currCtx, "New task is empty!", Toast.LENGTH_SHORT).show();
         }
 
-
     }
+
+    //load item list from sharedpreferences
+    private void loadItem(){
+        Gson gson = new Gson();
+        String json = sharedPref.getString("TaskData", "");
+        taskList = gson.fromJson(json, taskList.getClass());
+    }
+
 }
